b1566464189f6fc9f33ade4518e6de064994e88b
Sukhaman Singh <sukhamandev@gmail.com>
2024-10-15 17:17:05 -0400

    ListViewApp WindowGroup updated

--- ListView/ListView/ListView/Core/HTTPClient/HTTPClient.swift
+++ ListView/ListView/ListView/Core/HTTPClient/HTTPClient.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+protocol URLSessionProtcol {
+    func data(from request: URLRequest) async throws -> (Data, URLResponse)
+}
+
+class URLSessionWrapper: URLSessionProtcol {
+   
+    
+    private let session: URLSession
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func data(from request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await session.data(for: request)
+    }
+}
+
+protocol HTTPClientProtocol {
+    func performRequest(from request: URLRequest) async throws -> (Data, URLResponse)
+}
+
+
+class HTTPClient: HTTPClientProtocol {
+    private let session: URLSessionProtcol
+    init(session: URLSessionProtcol = URLSessionWrapper()) {
+        self.session = session
+    }
+    
+    func performRequest(from request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await session.data(from: request)
+    }
+}
