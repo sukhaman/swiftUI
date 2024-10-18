374e614dbcfb4fbcd31d2ee98e11cb70ee660b7f
Sukhaman Singh <sukhamandev@gmail.com>
2024-10-17 21:51:10 -0400

    Movie object created

---
+++ SwiftUIAnimation/SwiftUIAnimation/MovieCardList/Model/Movie.swift
@@ -0,0 +1,40 @@
+//
+//  Movie.swift
+//  SwiftUIAnimation
+//
+//  Created by Sukhaman Singh on 10/17/24.
+//
+
+import Foundation
+
+struct Movie: Identifiable, Codable {
+    let id: Int
+    let title: String
+    let originalTitle: String
+    let overview: String
+    let posterPath: String?
+    let backdropPath: String?
+    let releaseDate: String
+    let voteAverage: Double
+    let voteCount: Int
+    
+    let popularity: Double
+    let video: Bool
+    let adult: Bool
+    let originalLanguage: String
+    
+    // Computed properties to get full image URLs
+    var posterURL: URL? {
+        guard let posterPath = posterPath else { return nil }
+        return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")
+    }
+    
+    var backdropURL: URL? {
+        guard let backdropPath = backdropPath else { return nil }
+        return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath)")
+    }
+}
+
+struct MovieResult: Codable {
+    let results: [Movie]
+}
