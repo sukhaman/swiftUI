fb3f8c3f04429facf28df7f43cc92b7a90e788ab
Sukhaman Singh <sukhamandev@gmail.com>
2024-11-04 20:45:05 -0500

    ButtonStyeModifier and ContentView updated

--- ViewModifier/ViewModifier/ButtonStyleModifier.swift
+++ ViewModifier/ViewModifier/ButtonStyleModifier.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ButtonStyleModifier: ViewModifier {
+    var cornerRadius: CGFloat
     func body(content: Content) -> some View {
         content
             .padding()
@@ -19,13 +20,33 @@ struct ButtonStyleModifier: ViewModifier {
                 )
             )
             .foregroundColor(.white)
-            .cornerRadius(10)
+            .cornerRadius(cornerRadius)
             .shadow(color: .gray, radius: 5, x: 0, y: 5)
     }
 }
 
 extension View {
-    func customButtonStyle() -> some View {
-        self.modifier(ButtonStyleModifier())
+    func customButtonStyle(cornerRadius: CGFloat) -> some View {
+        self.modifier(ButtonStyleModifier(cornerRadius: cornerRadius))
+    }
+}
+
+struct GradientBorderModifier<S: Shape>: ViewModifier {
+    var shape: S
+    var gradient: LinearGradient
+    var lineWidth: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                shape
+                    .stroke(gradient, lineWidth: lineWidth)
+            )
+    }
+}
+
+extension View {
+    func gradientBorder<S: Shape>(_ shape: S, gradient: LinearGradient, lineWidth: CGFloat = 2) -> some View {
+        self.modifier(GradientBorderModifier(shape: shape, gradient: gradient, lineWidth: lineWidth))
     }
 }
--- ViewModifier/ViewModifier/ContentView.swift
+++ ViewModifier/ViewModifier/ContentView.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var notificationCount = 5
+    @State private var blurActive = false
     var body: some View {
-        VStack {
-           
-            Button {
-                // Button action
-            } label: {
-                Text("Show Error")
-                    .bold()
+        ZStack {
+            VStack {
+                HStack {
+                    Spacer()
+                    Image(systemName: "bell.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .badge(count: notificationCount, color: .red, offsetX: 15, offsetY: -15)
+                }
+                .padding()
+                Spacer()
+                
+                Button {
+                    notificationCount += 1
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30,height: 30)
+                }
+                .customButtonStyle(cornerRadius: 30)
+                
+                Spacer()
+                
+                
             }
-            .customButtonStyle()
-
+            .padding()
         }
-        .padding()
     }
 }
 
